bd7b191df75bbfc671f2475e222baf2c8f634d2b
milenamoran006 <milenajamnia@gmail.com>
2024-10-04 13:31:17 +0000

    se agrego el extends a la clase gerente

---
+++ BuilderPatternTrabajador/Gerente.java
@@ -0,0 +1,13 @@
+public class Gerente extends Trabajador{
+    private int id;
+    private String nombre;
+    private double salarioIntegral;    
+
+    public Gerente(int id, String nombre, double salarioIntegral){
+        this.id=id;
+        this.nombre=nombre;
+        this.salarioIntegral=salarioIntegral;
+    }
+    public double pagar(){
+        return salarioIntegral;
+    }
