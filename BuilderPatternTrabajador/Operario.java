17955582561b1120cd84f6213e6f631151e5ce84
milenamoran006 <milenajamnia@gmail.com>
2024-10-04 13:27:11 +0000

    Creacion de la clase Operario

---
+++ BuilderPatternTrabajador/Operario.java
@@ -0,0 +1,14 @@
+public class Operario extends Trabajador{
+    private int id;
+    private String nombre;
+    private double horas;
+    private final double valor_hora=5500.0;
+
+    public Operario(int id, String nombre, double horas){
+        this.id=id;
+        this.nombre=nombre;
+        this.comision=horas;        
+    }
+    public double pagar(){
+        return comision*venta;
+    }
