2bc86ed33286e873f701e806bfed0219262e7bee
milenamoran006 <milenajamnia@gmail.com>
2024-10-11 14:12:19 +0000

    clase cancha

---
+++ SistemaReservaCancha/Cancha.java
@@ -0,0 +1,14 @@
+public class Cancha {
+    private String tipoCancha;
+    private double precio;
+    private String caracteristicas;
+    private List<String> disponibilidad;
+
+    public Cancha(String tipoCancha, double precio, String caracteristicas, list<String> disponibilidad){
+        this.tipoCancha = tipoCancha;
+        this.precio = precio;
+        this.caracteristicas = caracteristicas;
+        this.disponibilidad = disponibilidad;
+    }
+    public 
+}
